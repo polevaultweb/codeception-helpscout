2f0d4b0ec6ced1b40b8878866910029163fc9c2a
Iain <iain@polevaultweb.com>
2019-10-24 18:39:56 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,72 @@
+Codeception Help Scout
+==========
+
+A Help Scout email module for Codeception.
+
+## Installation
+You need to add the repository into your composer.json file
+
+```bash
+    composer require --dev polevaultweb/codeception-helpscout
+```
+
+## Usage
+
+You can use this module as any other Codeception module, by adding 'HelpScout' to the enabled modules in your Codeception suite configurations.
+
+### Add Drip to your list of modules
+
+```yml
+modules:
+    enabled:
+        - HelpScout
+ ```  
+
+### Setup the configuration variables
+
+```yml
+    config:
+        HelpScout:
+            app_id: '%HELPSCOUT_APP_ID%'
+            app_secret: '%HELPSCOUT_APP_SECRET%'
+ ```     
+ 
+Update Codeception build
+  
+  ```bash
+  codecept build
+  ```
+  
+### Supports
+
+This Codeception Module implements the required methods to test emails using the [Codeception Email Testing Framework](https://github.com/ericmartel/codeception-email) with [Help Scout](https://www.helpscout.com/referral/?code=TzdWZDcwbU0xa2Z6Rnh6c2s4TGxHTmR6L3ptd3J2dzlpb210L0RzUCtjbWVvUT09OnJENTdzaHA2RE5XbGVDa2E)
+
+### Added Methods
+This Module adds a few public methods for the user, such as:
+```
+dontHaveEmailEmail()
+```
+Deletes an email in Help Scout
+
+```
+fetchEmails()
+```
+Fetches all conversations from Help Scout, and assigns them to the current and unread inboxes
+
+```
+openNextUnreadEmail()
+```
+Pops the most recent unread email and assigns it as the email to conduct tests on
+
+### Usage
+
+```php
+$I = new AcceptanceTester( $scenario );
+
+$I->fetchEmails( 12345 );
+$I->openNextUnreadEmail();
+$I->seeInOpenedEmailSubject( 'Thank you' );
+$I->seeInOpenedEmailSender( 'john@doe.com' );
+$I->seeInOpenedEmailBody( 'Hey, thanks for the great product' );
+```
+
---
+++ src/HelpScout.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace Codeception\Module;
+
+use Codeception\Module;
+use HelpScout\Api\ApiClientFactory;
+use HelpScout\Api\Conversations\ConversationFilters;
+use HelpScout\Api\Conversations\ConversationRequest;
+use HelpScout\Api\Conversations\EmailConversation;
+
+class HelpScout extends Module {
+
+	use \Codeception\Email\TestsEmails;
+	use \Codeception\Email\EmailServiceProvider;
+
+	protected $client;
+
+	/**
+	 * Conversations
+	 *
+	 * @var array
+	 */
+	protected $fetchedEmails;
+
+	/**
+	 * Currently selected set of email headers to work with
+	 *
+	 * @var array
+	 */
+	protected $currentInbox;
+	/**
+	 * Starts as the same data as the current inbox, but items are removed as they're used
+	 *
+	 * @var array
+	 */
+	protected $unreadInbox;
+
+	/**
+	 * Contains the currently open email on which test operations are conducted
+	 *
+	 * @var mixed
+	 */
+	protected $openedEmail;
+
+	/**
+	 * Codeception exposed variables
+	 *
+	 * @var array
+	 */
+	protected $config = array( 'app_id', 'app_secret', 'mailbox_id' );
+
+	public function _initialize() {
+		$client       = ApiClientFactory::createClient();
+		$this->client = $client->useClientCredentials( $this->config['app_id'], $this->config['app_secret'] );
+	}
+
+	/**
+	 * Fetch Emails
+	 *
+	 * Accessible from tests, fetches all emails
+	 *
+	 * @param null $mailboxID
+	 */
+	public function fetchEmails( $mailboxID = null ) {
+
+		$mailboxID = is_null( $mailboxID ) ? $this->config['mailbox_id'] : $mailboxID;
+
+		$this->fetchedEmails = array();
+		try {
+			$request = (new ConversationRequest)
+				->withMailbox()
+				->withThreads();
+
+			$filters = ( new ConversationFilters() )->withMailbox( $mailboxID )
+			                                        ->withStatus( 'open' )
+			                                        ->withQuery( 'assigned:"Unassigned"' )
+			                                        ->withSortField( 'createdAt' )
+			                                        ->withSortOrder( 'asc' );
+
+			$conversations = $this->client->conversations()->list( $filters, $request );
+			$this->fetchedEmails = $conversations->toArray();
+		} catch ( Exception $e ) {
+			$this->fail( 'Exception: ' . $e->getMessage() );
+		}
+
+		// by default, work on all emails
+		$this->setCurrentInbox( $this->fetchedEmails  );
+	}
+
+	/**
+	 * Set Current Inbox
+	 *
+	 * Sets the current inbox to work on, also create a copy of it to handle unread emails
+	 *
+	 * @param array $inbox Inbox
+	 */
+	protected function setCurrentInbox( $inbox ) {
+		$this->currentInbox = $inbox;
+		$this->unreadInbox  = $inbox;
+	}
+
+	/**
+	 * Get Opened Email
+	 *
+	 * Main method called by the tests, providing either the currently open email or the next unread one
+	 *
+	 * @param bool $fetchNextUnread Goes to the next Unread Email
+	 * @return mixed Returns a JSON encoded Email
+	 */
+	public function getOpenedEmail($fetchNextUnread = FALSE)
+	{
+		if($fetchNextUnread || $this->openedEmail == NULL)
+		{
+			$this->openNextUnreadEmail();
+		}
+		return $this->openedEmail;
+	}
+
+	/**
+	 * Get Most Recent Unread Email
+	 *
+	 * Pops the most recent unread email, fails if the inbox is empty
+	 *
+	 * @return mixed Returns a JSON encoded Email
+	 */
+	protected function getMostRecentUnreadEmail() {
+		if ( empty( $this->unreadInbox ) ) {
+			$this->fail( 'Unread Inbox is Empty' );
+		}
+		$email = array_shift( $this->unreadInbox );
+
+		return $email;
+	}
+
+	/**
+	 * Open Next Unread Email
+	 *
+	 * Pops the most recent unread email and assigns it as the email to conduct tests on
+	 */
+	public function openNextUnreadEmail()
+	{
+		$this->openedEmail = $this->getMostRecentUnreadEmail();
+	}
+
+	/**
+	 * Get Email Subject
+	 *
+	 * Returns the subject of an email
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string Subject
+	 */
+	protected function getEmailSubject($email)
+	{
+		return $email->getSubject();
+	}
+
+	/**
+	 * Get Email Body
+	 *
+	 * Returns the body of an email
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string Body
+	 */
+	protected function getEmailBody($email)
+	{
+		return $email->getThreads()->toArray()[0]->getText();
+	}
+
+	/**
+	 * Get Email To
+	 *
+	 * Returns the string containing the persons included in the To field
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string To
+	 */
+	protected function getEmailTo( $email ) {
+		return $email->getMailbox()->getEmail();
+	}
+
+	/**
+	 * Get Email CC
+	 *
+	 * Returns the string containing the persons included in the CC field
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string CC
+	 */
+	protected function getEmailCC($email)
+	{
+		return implode( ',', $email->getCC() );
+	}
+	/**
+	 * Get Email BCC
+	 *
+	 * Returns the string containing the persons included in the BCC field
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string BCC
+	 */
+	protected function getEmailBCC($email)
+	{
+		return implode( ',', $email->getBCC() );
+	}
+
+	/**
+	 * Get Email Sender
+	 *
+	 * Returns the string containing the sender of the email
+	 *
+	 * @param EmailConversation $email Email
+	 * @return string Sender
+	 */
+	protected function getEmailSender($email)
+	{
+		return $email->getCustomer()->getFirstEmail();
+	}
+
+	/**
+	 * @param EmailConversation $email
+	 */
+	public function dontHaveEmailEmail( $email ) {
+		$this->client->conversations()->delete( $email->getId() );
+	}
+}
